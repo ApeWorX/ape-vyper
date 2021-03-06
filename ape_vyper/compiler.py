4df49ecc68c1cd8f3020812aa5d74dc673e2d311
Shade Undertree <72354637+ShadeUndertree@users.noreply.github.com>
2021-03-06 15:23:56 -0800

    feat: add compilation via vvm
    chore: add version to .gitignore

--- ape_vyper/compiler.py
+++ ape_vyper/compiler.py
@@ -1,9 +1,11 @@
+import os
 from typing import Dict
 from pathlib import Path
 
 from ape.plugins.compiler_api import CompilerAPI
 
 import click
+import vvm
 
 
 class VyperCompiler(CompilerAPI):
@@ -15,6 +17,15 @@ class VyperCompiler(CompilerAPI):
     def extension(self) -> str:
         return ".vy"
 
+    # convert dict to class defined in ape core
+    # arguments are folder, and compiler settings (from config)
     def compile(self, contracts_folder: Path) -> Dict:
-        click.echo(f"vyper plugin compile called, compiling {contracts_folder}")
+        click.echo(f"compiling {contracts_folder}")
+
+        contracts = [os.path.join(contracts_folder, c) for c in os.listdir(contracts_folder) if c.endswith(".vy")]
+
+        result = vvm.compile_files(contracts)
+
         click.echo("vyper compilation finished")
+
+        return result
