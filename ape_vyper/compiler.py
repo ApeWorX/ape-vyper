f1c4fd5fbb28cbd266f66a49c04ff97b0dbdb16f
Shade Undertree <72354637+ShadeUndertree@users.noreply.github.com>
2021-03-15 17:21:52 -0700

    feat: add manifest-based compilation

--- ape_vyper/compiler.py
+++ ape_vyper/compiler.py
@@ -3,9 +3,12 @@ from typing import Dict
 from pathlib import Path
 
 from ape.plugins.compiler_api import CompilerAPI
+from ape.ethpm import PackageManifest, ContractType, Bytecode
 
 import click
 import vvm
+
+import json
 
 
 class VyperCompiler(CompilerAPI):
@@ -14,18 +17,46 @@ class VyperCompiler(CompilerAPI):
         return "vyper"
 
     @classmethod
-    def extension(self) -> str:
-        return ".vy"
+    def handles(self, contract_type: str) -> bool:
+        return contract_type == "vy" or contract_type == ".vy" or contract_type == "vyper"
 
-    # convert dict to class defined in ape core
-    # arguments are folder, and compiler settings (from config)
-    def compile(self, contracts_folder: Path) -> Dict:
-        click.echo(f"compiling {contracts_folder}")
+    def compile(self, pkg_manifest: PackageManifest) -> PackageManifest:
+        if pkg_manifest.name:
+            click.echo(f"compiling {pkg_manifest.name} vyper contracts")
+        else:
+            click.echo("compiling vyper contracts")
 
-        contracts = [os.path.join(contracts_folder, c) for c in os.listdir(contracts_folder) if c.endswith(".vy")]
+        if not pkg_manifest.sources:
+            click.echo("nothing to compile")
+            return
 
-        result = vvm.compile_files(contracts)
+        for (name, source) in pkg_manifest.sources.items():
+            if source.type != "vyper":
+                continue
+
+            if not source.content:
+                # if error, e.g. no URL, error to console, let other 'good' contracts finish - don't halt entire process
+                # TODO checksum checking
+                source.load_content()
+
+            result = vvm.compile_source(source.content)
+
+            # o = source.urls[0].split("/")[-1]
+            # temp = f"/home/shade/source/ape-vyper/{o}"
+            # with open(temp, "w") as fp:
+            #     json.dump(result, fp, indent=4)
+
+            result = result["<stdin>"]
+            db = Bytecode(result["bytecode"], None, None)
+            rb = Bytecode(result["bytecode_runtime"], None, None)
+            abi = result["abi"]
+            userdoc = result["userdoc"]
+            devdoc = result["devdoc"]
+            contract = ContractType(name, name, db, rb, abi, userdoc, devdoc)
+
+            if not pkg_manifest.contractTypes:
+                pkg_manifest.contractTypes = []
+
+            pkg_manifest.contractTypes.append(contract)
 
         click.echo("vyper compilation finished")
-
-        return result
