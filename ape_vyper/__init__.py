80db4d80f13b8486eb1fa6ac1e7a9d1bec383761
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2021-04-19 16:36:14 -0400

    refactor: updates per https://github.com/ApeWorX/ape/pull/26

--- ape_vyper/__init__.py
+++ ape_vyper/__init__.py
@@ -5,4 +5,4 @@ from .compiler import VyperCompiler
 
 @plugins.register(plugins.CompilerPlugin)
 def register_compiler():
-    return VyperCompiler
+    return (".vy",), VyperCompiler
--- ape_vyper/compiler.py
+++ ape_vyper/compiler.py
@@ -1,7 +1,9 @@
+from pathlib import Path
+
 import click
-import vvm
+import vvm  # type: ignore
 from ape.api.compiler import CompilerAPI
-from ape.package import Bytecode, ContractType, PackageManifest
+from ape.types import Bytecode, ContractType
 
 
 class VyperCompiler(CompilerAPI):
@@ -9,46 +11,18 @@ class VyperCompiler(CompilerAPI):
     def name(self) -> str:
         return "vyper"
 
-    @classmethod
-    def handles(self, contract_type: str) -> bool:
-        return contract_type == "vy" or contract_type == ".vy" or contract_type == "vyper"
+    def compile(self, contract_filepath: Path) -> ContractType:
+        click.echo(f"Compiling '{contract_filepath}'")
 
-    def compile(self, pkg_manifest: PackageManifest) -> PackageManifest:
-        if pkg_manifest.name:
-            click.echo(f"compiling {pkg_manifest.name} vyper contracts")
-        else:
-            click.echo("compiling vyper contracts")
+        result = vvm.compile_source(contract_filepath.read_text())
 
-        if not pkg_manifest.sources:
-            click.echo("nothing to compile")
-            return pkg_manifest
-
-        # TODO: explore async loop here
-        for (name, source) in pkg_manifest.sources.items():
-            if source.type != "vyper":
-                continue
-
-            if not source.content:
-                # TODO if error, e.g. no URL, error to console, let other
-                # 'good' contracts finish - don't halt entire process
-                # TODO checksum checking
-                source.load_content()
-
-            result = vvm.compile_source(source.content)
-
-            result = result["<stdin>"]
-            db = Bytecode(result["bytecode"], None, None)
-            rb = Bytecode(result["bytecode_runtime"], None, None)
-            abi = result["abi"]
-            userdoc = result["userdoc"]
-            devdoc = result["devdoc"]
-            contract = ContractType(name, name, db, rb, abi, userdoc, devdoc)
-
-            # TODO: avoid this via default arg w/ dataclass
-            if not pkg_manifest.contractTypes:
-                pkg_manifest.contractTypes = []
-
-            pkg_manifest.contractTypes.append(contract)
-
-        click.echo("vyper compilation finished")
-        return pkg_manifest
+        result = result["<stdin>"]
+        return ContractType(
+            contractName=contract_filepath.name,
+            sourceId=contract_filepath,
+            deploymentBytecode=Bytecode(result["bytecode"]),  # type: ignore
+            runtimeBytecode=Bytecode(result["bytecode_runtime"]),  # type: ignore
+            abi=result["abi"],
+            userdoc=result["userdoc"],
+            devdoc=result["devdoc"],
+        )
