9f1536ce47a3cbc38b58324fd0934519af6bd568
sabotagebeats <27985126+sabotagebeats@users.noreply.github.com>
2021-07-12 14:11:18 -0700

    updated test coverage to except Abort

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,10 +1,13 @@
+from ape.utils import Abort
+
+
 def test_integration(project):
     assert "contract" in project.contracts
     assert "contract_no_pragma" in project.contracts
 
+
 def test_failure(project):
     try:
         assert "contract_with_error" in project.contracts
-    except VyperError:
+    except Abort:
         pass
-
