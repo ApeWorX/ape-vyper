aa367baa62166ea4b2c0a4f8aaca9f68170fae72
sabotagebeats <27985126+sabotagebeats@users.noreply.github.com>
2021-07-12 13:34:34 -0700

    updated  to add test_integration support for contract_with_errory.vy

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,3 +1,4 @@
 def test_integration(project):
     assert "contract" in project.contracts
     assert "contract_no_pragma" in project.contracts
+    assert "contract_with_error" in project.contracts
