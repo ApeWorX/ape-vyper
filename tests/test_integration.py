4377b16f2ecff83d42b1f329a200a818c9dbbc38
sabotagebeats <27985126+sabotagebeats@users.noreply.github.com>
2021-07-13 14:56:45 -0700

    added withpytest.raises(Abort)

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,12 +1,11 @@
 from ape.utils import Abort
+import pytest
 
-# def test_integration(project):
-#     assert "contract" in project.contracts.passing_contracts
+# def test_integration(passing_project):
+#     assert "contract" in passing_project
 #     assert "contract_no_pragma" in project.contracts.passing_contracts
 
 
-def test_failure(project):
-    try:
-        assert "contract_with_error" in project.contracts.erroring_contracts
-    except Abort:
-        pass
+def test_failure(failing_project):
+    with pytest.raises(Abort):
+        assert "contract_with_error" in failing_project
