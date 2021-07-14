c64653b4fe549705a098a2410d3f309d1aea5346
sabotagebeats <27985126+sabotagebeats@users.noreply.github.com>
2021-07-14 09:51:49 -0700

    flake8 isort black

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,17 +1,17 @@
-from ape.types import ContractType
 from pathlib import Path
+
 import pytest
 
 PASSING_CONTRACTS_FOLDER = Path(__file__).parent / "contracts" / "passing_contracts"
 FAILING_CONTRACTS_FOLDER = Path(__file__).parent / "contracts" / "failing_contracts"
 
-@pytest.mark.parametrize("path",PASSING_CONTRACTS_FOLDER.glob("*.vy"))
-def test_pass(path,compiler):
+
+@pytest.mark.parametrize("path", PASSING_CONTRACTS_FOLDER.glob("*.vy"))
+def test_pass(path, compiler):
     assert compiler.compile([path])
 
 
-@pytest.mark.parametrize("path",FAILING_CONTRACTS_FOLDER.glob("*.vy"))
-def test_failure(path,compiler):
+@pytest.mark.parametrize("path", FAILING_CONTRACTS_FOLDER.glob("*.vy"))
+def test_failure(path, compiler):
     with pytest.raises(Exception):
         compiler.compile([path])
-
