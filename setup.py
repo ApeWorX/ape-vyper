4b947e2149c827d17fc6879e86e6d4181742500f
unparalleled-js <yingthi@live.com>
2022-05-12 07:28:59 -0500

    chore: upgrade lint

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ extras_require = {
     ],
     "lint": [
         "black>=22.3.0,<23.0",  # auto-formatter and linter
-        "mypy>=0.910,<1.0",  # Static type analyzer
+        "mypy>=0.950,<1.0",  # Static type analyzer
         "flake8>=3.9.2,<4.0",  # Style linter
-        "isort>=5.9.3,<6.0",  # Import sorting linter
+        "isort>=5.10.1,<6.0",  # Import sorting linter
     ],
     "release": [  # `release` GitHub Action job uses this
         "setuptools",  # Installation tool
