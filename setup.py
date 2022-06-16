d51b27674e4f24af33bb90f91152bc9be5e9b23c
15219900+Ninjagod1251@users.noreply.github.com <15219900+Ninjagod1251@users.noreply.github.com>
2022-06-17 03:08:28 +0700

    fix: mypy

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ extras_require = {
     ],
     "lint": [
         "black>=22.3.0,<23.0",  # auto-formatter and linter
-        "mypy>=0.910,<1.0",  # Static type analyzer
+        "mypy>=0.950,<1.0",  # Static type analyzer
         "flake8>=3.9.2,<4.0",  # Style linter
         "isort>=5.10.1,<6.0",  # Import sorting linter
     ],
