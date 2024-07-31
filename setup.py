10156938b11bcb0c067ff18403c75ad657591670
Dalena <mimidalena@gmail.com>
2024-07-31 12:33:00 -0500

    feat: shibuya theme

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ extras_require = {
         "sphinx_rtd_theme>=1.2.0,<2",  # Readthedocs.org theme
         "sphinxcontrib-napoleon>=0.7",  # Allow Google-style documentation
         "sphinx-plausible>=0.1.2,<0.2",
+        "shibuya",
     ],
     "release": [  # `release` GitHub Action job uses this
         "setuptools",  # Installation tool
